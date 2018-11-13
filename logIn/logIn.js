ab13991b65c59ac2b782a07fcd7be5a2520b1e65
gimtommang11 <sh20058@naver.com>
2018-11-13 18:14:33 +0900

    commited

---
+++ logIn/logIn.js
@@ -0,0 +1,2 @@
+var req = new XMLHttpRequest();
+req.open('POST', 'dms.istruly.sexy:792/', true);
---
+++ signUp/signUp.js
@@ -0,0 +1,11 @@
+    function signup(){
+    let Id = document.getElementById("id").value;
+    let Pw = document.getElementById("password").value;
+    console.log(Id);
+    console.log(Pw);
+    
+    /*let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'dms.istruly.sexy:792/signup', true);        //요청합니다. http요구방식, 요구하고자 하는 url,true(비동기식)
+    xhr.setRequestHeader("Content-type", "application/json")  */      //html폼으로 post데이터를 보냄
+    
+}
