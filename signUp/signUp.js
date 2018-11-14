c80e4905bc6c50dcd63f739f369cb214435b1554
gimtommang11 <sh20058@naver.com>
2018-11-14 23:40:16 +0900

    signup request

--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,11 +1,30 @@
-    function signup(){
-    let Id = document.getElementById("id").value;
-    let Pw = document.getElementById("password").value;
-    console.log(Id);
-    console.log(Pw);
-    
-    /*let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'dms.istruly.sexy:792/signup', true);        //요청합니다. http요구방식, 요구하고자 하는 url,true(비동기식)
-    xhr.setRequestHeader("Content-type", "application/json")  */      //html폼으로 post데이터를 보냄
-    
+function signup(){
+    const id = document.getElementById("id").value;
+    const password = document.getElementById("pw").value;
+    const singupbtn = document.getElementById("signUpbtn")
+
+    console.log(id);
+    console.log(password);
+
+    const xhr = new XMLHttpRequest();
+
+    const data = {
+        id,
+        password
+    };
+
+    xhr.open('POST', 'http://ec2.istruly.sexy:792/signup',false);
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(JSON.stringify(data));
+    console.log(xhr.status);
+
+    if(xhr.status === 201){
+        console.log("가입에 성공하였습니다");
+        alert("가입 성공"); 
+        location.href="../main/main.html";
+    }
+    else if(xhr.status === 204){
+        console.log("중복 ID");
+        alert("중복 ID입니다");
+    }
 }
