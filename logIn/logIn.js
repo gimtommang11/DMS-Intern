23b534a6ccc95774ff5ba8f3385dbe3f03a2b691
gimtommang11 <sh20058@naver.com>
2018-11-14 23:41:29 +0900

    login request

--- logIn/logIn.js
+++ logIn/logIn.js
@@ -1,2 +1,26 @@
-var req = new XMLHttpRequest();
-req.open('POST', 'dms.istruly.sexy:792/', true);
+function logIn(){
+    const id = document.getElementById("id").value;
+    const password = document.getElementById("pw").value;
+    const singupbtn = document.getElementById("signupbtn");
+
+    console.log(id , pw);
+
+    let xhr = new XMLHttpRequest();
+    
+    const parameters = {
+        id,
+        password
+    };
+
+    xhr.open("POST", "http://ec2.istruly.sexy:792/login", false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(parameters));
+
+    if(xhr.status === 200){
+        alert("로그인이 성공하엿습니다");
+    }
+    else if(xhr.status === 401){
+        alert("로그인에 실피하였습니다");
+    }
+
+}
