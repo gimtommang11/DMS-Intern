d3c3c644a6f4cbc53dc94155d12be1c9163721fd
gimtommang11 <sh20058@naver.com>
2018-11-21 20:09:32 +0900

    add herf in td

--- main/main.js
+++ main/main.js
@@ -15,6 +15,7 @@ function getPosts(){
         showPosts(posts);       //showPosts함수 호출
         console.log(posts);
 
+        console.log("http://ec2.istruly.sexy:792/"+title);
         const table = document.getElementById("table");
     }
     else if(xhr.status === 204){
@@ -27,23 +28,34 @@ function getPosts(){
 
 function showPosts(posts) {     
     const tbody = document.getElementById('table-body');
-    for(let post of posts) {        //for of - 이터러블 객체(반복할 수 있게 하는 객체 ex)array, String..)를 순회할 때 사용, 값을 return함
+    let storage = localStorage;
+    let postId = new Array();
+    let i = 0;
+
+    for(let post in posts) {        //for of - 이터러블 객체(반복할 수 있게 하는 객체 ex)array, String..)를 순회할 때 사용, 값을 return함
         const tr = document.createElement("tr");    //tr엘리먼트를 추가
-        tr.herf = "../post./post.html";
         /*tr.innerHTML = `
            <td>${post.title}</td>
             <td>${post.author}</td>
         `;*/
-        tr.innerHTML += /*"<td>" +post.title+ "</td>";*/"<td>"+"<a href='"+"../post/post.html"+"'>"+post.title+"</a>"+"</td>"   //+"<a href='"+post.title+"'>Google</a>";
+        tr.innerHTML +=
+        "<td>"+"<a href='"+"../post/post.html'>"+i+"\t"+post.title+"</a>"+"</td> <td>"+post.author+"</td>";
         tbody.appendChild(tr);      //appendChild:노드를 노드의 마지막 자식으로 추가
         //노드란? <a></a> / <div></div>같은 것
+        
+        postId.push(post.postId);
+        console.log(postId);
+        //storage.setItem("postId",JSON.stringify(postId));
+        i++;
     }
+    console.log(postId);
+    storage.setItem("postId", JSON.stringify(postId));
+    console.log(storage.postId);
 }
 
-function getPostId(index) {     //postid 얻는다
+/*function getPostId(index) {     //postid 얻는다
     return posts[index].postId;
 }           
 
-/*let storage = localStorage;
-        storage.setItem('postId', post.postId);
-        console.log(postId);*/
+
+//postid 배열에서 분리하는것 물어보기
